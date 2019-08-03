1f774082608f61253fbca27cd8dfe3fd9ccc7877
javarce <bogeresiraje25@gmail.com>
2019-08-03 13:49:33 +0300

    Changed db url from a local to a remote url.

--- main/config.py
+++ main/config.py
@@ -23,7 +23,7 @@ class Configuration(object):
 # prod config
 class Configuration(object):
 	APPLICATION_DIR = os.path.dirname(os.path.realpath(__file__))
-	SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:12345@localhost:5432/id_recovery'
+	SQLALCHEMY_DATABASE_URI = 'postgres://tdfteaxnpgqhnz:bdd279f1ad600575e21e20116bcac8343871ccd78684e07450578bff258b433a@ec2-174-129-226-234.compute-1.amazonaws.com:5432/d1g0488scsfqi6'
 	SQLALCHEMY_TRACK_MODIFICATIONS = True
 	STATIC_DIR = os.path.join(APPLICATION_DIR, 'api/uploads')
 	SECRET_KEY = 'hgdfydUH#&SF@Fhytdt5785'
