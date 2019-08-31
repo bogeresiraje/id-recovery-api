b85fde0759d97fcb73ecedf03aac8b9c8f021b2f
javarce <bogeresiraje25@gmail.com>
2019-08-31 20:54:30 +0300

    Fix database url

--- main/config.py
+++ main/config.py
@@ -2,35 +2,35 @@ import os
 
 
 
-# dev config
-class Configuration(object):
-	APPLICATION_DIR = os.path.dirname(os.path.realpath(__file__))
-	SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:12345@localhost:5432/id_recovery'
-	SQLALCHEMY_TRACK_MODIFICATIONS = True
-	STATIC_DIR = os.path.join(APPLICATION_DIR, 'api/uploads')
-	# Path for remporary images -> for face detection
-	TEMP_IMAGES = os.path.join(APPLICATION_DIR, 'api/face/temp_images')
-	# Path forOpenCV haar cascades
-	CASCADES_DIR = os.path.join(APPLICATION_DIR, 'api/face/cascades')
-	# Path for data sets.
-	DATA_SETS_DIR = os.path.join(APPLICATION_DIR, 'api/face/data_sets')
-	SECRET_KEY = 'hgdfydUH#&SF@Fhytdt5785'
-	DEBUG = True
+# # dev config
+# class Configuration(object):
+# 	APPLICATION_DIR = os.path.dirname(os.path.realpath(__file__))
+# 	SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:12345@localhost:5432/id_recovery'
+# 	SQLALCHEMY_TRACK_MODIFICATIONS = True
+# 	STATIC_DIR = os.path.join(APPLICATION_DIR, 'api/uploads')
+# 	# Path for remporary images -> for face detection
+# 	TEMP_IMAGES = os.path.join(APPLICATION_DIR, 'api/face/temp_images')
+# 	# Path forOpenCV haar cascades
+# 	CASCADES_DIR = os.path.join(APPLICATION_DIR, 'api/face/cascades')
+# 	# Path for data sets.
+# 	DATA_SETS_DIR = os.path.join(APPLICATION_DIR, 'api/face/data_sets')
+# 	SECRET_KEY = 'hgdfydUH#&SF@Fhytdt5785'
+# 	DEBUG = True
 
-	# Mail configs
-	MAIL_SERVER = 'smtp.gmail.com'
-	MAIL_PORT = 465
-	MAIL_USERNAME = 'ronniwallace2017@gmail.com'
-	MAIL_PASSWORD = 'wilishere06'
-	MAIL_USE_TLS = False
-	MAIL_USE_SSL = True
+# 	# Mail configs
+# 	MAIL_SERVER = 'smtp.gmail.com'
+# 	MAIL_PORT = 465
+# 	MAIL_USERNAME = 'ronniwallace2017@gmail.com'
+# 	MAIL_PASSWORD = 'wilishere06'
+# 	MAIL_USE_TLS = False
+# 	MAIL_USE_SSL = True
 
 
-'''
+
 # prod config
 class Configuration(object):
 	APPLICATION_DIR = os.path.dirname(os.path.realpath(__file__))
-	SQLALCHEMY_DATABASE_URI = 'postgres://tdfteaxnpgqhnz:bdd279f1ad600575e21e20116bcac8343871ccd78684e07450578bff258b433a@ec2-174-129-226-234.compute-1.amazonaws.com:5432/d1g0488scsfqi6'
+	SQLALCHEMY_DATABASE_URI = 'postgres://xcmtwtuzteggyo:51c2a3b9dd5708598b569ba82192da488639800ed6a6100b824b22c69c0bbe3d@ec2-23-21-148-223.compute-1.amazonaws.com:5432/dfet9ct7nt30sn'
 	SQLALCHEMY_TRACK_MODIFICATIONS = True
 	STATIC_DIR = os.path.join(APPLICATION_DIR, 'api/uploads')
 	SECRET_KEY = 'hgdfydUH#&SF@Fhytdt5785'
@@ -43,4 +43,3 @@ class Configuration(object):
 	MAIL_PASSWORD = 'wilishere06'
 	MAIL_USE_TLS = False
 	MAIL_USE_SSL = True
-'''
