d9b2caa1bebd0fa2d0e46d797c6609a6f348f29c
javarce <bogeresiraje25@gmail.com>
2019-08-31 22:53:00 +0300

    Fix prdtn config

--- main/config.py
+++ main/config.py
@@ -33,6 +33,12 @@ class Configuration(object):
 	SQLALCHEMY_DATABASE_URI = 'postgres://xcmtwtuzteggyo:51c2a3b9dd5708598b569ba82192da488639800ed6a6100b824b22c69c0bbe3d@ec2-23-21-148-223.compute-1.amazonaws.com:5432/dfet9ct7nt30sn'
 	SQLALCHEMY_TRACK_MODIFICATIONS = True
 	STATIC_DIR = os.path.join(APPLICATION_DIR, 'api/uploads')
+	# Path for remporary images -> for face detection
+	TEMP_IMAGES = os.path.join(APPLICATION_DIR, 'api/face/temp_images')
+ 	# Path forOpenCV haar cascades
+ 	CASCADES_DIR = os.path.join(APPLICATION_DIR, 'api/face/cascades')
+	# Path for data sets.
+	DATA_SETS_DIR = os.path.join(APPLICATION_DIR, 'api/face/data_sets')
 	SECRET_KEY = 'hgdfydUH#&SF@Fhytdt5785'
 	DEBUG = True
 
